6061dc8d88b6da50cc706c36c3c2c2af9a6f2d8e
Shaluranijnv <69577028+Shaluranijnv@users.noreply.github.com>
2021-04-03 18:16:03 +0530

    Update main.js

--- Trial/js/main.js
+++ Trial/js/main.js
@@ -11,9 +11,16 @@ $(document).ready(function(){
 $(window).on("scroll",function(){
   if($(this).scrollTop()>70){
     $("nav.container-fluid").addClass("navbar-shrink");
+   
+    $(".download").css("display","block");
+    $(".download").css("text-align","center");
+    $(".now").css("display","none");
   }
   else{
     $("nav.container-fluid").removeClass("navbar-shrink");
+   
+    $(".download").css("display","none");
+    $(".now").css("display","block");
   }
 });
 });
